eda2de43d58d4c0ad5bfc3b346be95363d321454
eduarda12345 <57159917+eduarda12345@users.noreply.github.com>
2022-06-30 14:46:38 -0300

    Faltam alguma ajuste

---
+++ compass/TDD's/src/calculadora.rb
@@ -0,0 +1,11 @@
+class Calculadora
+    def soma(a, b)  #calculo de somar
+       a + b
+end
+def subtraia(a,b)  #calculo de subtrair
+  a - b
+end
+def multiplicacao(a,b)  #calculo de multiplicacao
+  a * b
+end
+end
